4cca88b4e6c1a4fe045ebfc406fcce3d0777cd42
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-23 16:43:25 +0200

    Bootstrap import and start working on the layout

--- index.php
+++ index.php
@@ -49,23 +49,27 @@ $db = [
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Domande frequenti - Privacy e termini - Google</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <?php
+<main>
+
+<div class="container">
+<?php
        foreach ($db as $key) {
-           # code...
-           echo $key['question'];
-           echo "<br>";
+           echo "<h3>{$key['question']}</h3>";
            $txt=explode(";",$key['answer']);
            foreach ($txt as $p) {
-               echo $p;
-               echo "<br>";
+               echo "<p>{$p}</p>";
            }
        }
-    ?>
-    
+?>
+</div>
+
+</main>
+ 
 </body>
 
 </html>
